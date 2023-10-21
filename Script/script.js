2a1050f0f6173499f9a299c9f46224433c82847a
Joanibi Ribeiro <101252091+Joanibi@users.noreply.github.com>
2023-10-21 19:23:33 -0300

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,58 @@ window.onload = function () {
     window.addEventListener("contextmenu", e => e.preventDefault());
 } 
 
+(function(b) {
+    var a = {
+        version: "0.0.1",
+        history_api: typeof history.pushState !== "undefined",
+        init: function() {
+            b.location.hash = "#no-back";
+            a.configure();
+        },
+        hasChanged: function() {
+            if (b.location.hash == "#no-back") {
+                b.location.hash = "#";
+                b.location.href = "https://go.perfectpay.com.br/PPU38CN6PO2"; //LINK PARA REDIRECIONAR AQUI
+            }
+        },
+        checkCompat: function() {
+            if (b.addEventListener) {
+                b.addEventListener("hashchange", a.hasChanged, false);
+            } else {
+                if (b.attachEvent) {
+                    b.attachEvent("onhashchange", a.hasChanged);
+                } else {
+                    b.onhashchange = a.hasChanged;
+                }
+            }
+        },
+        configure: function() {
+            if (b.location.hash == "#no-back") {
+                if (this.history_api) {
+                    history.pushState(null, "", "#");
+                } else {
+                    b.location.hash = "#";
+                    b.location.href = "https://go.perfectpay.com.br/PPU38CN6PO2"; //LINK PARA REDIRECIONAR AQUI
+                }
+            }
+            a.checkCompat();
+            a.hasChanged();
+        }
+    };
+    if (typeof define === "function" && define.amd) {
+        define(function() {
+            return a;
+        });
+    } else {
+        if (typeof module === "object" && module.exports) {
+            module.exports = a;
+        } else {
+            b.noback = a;
+        }
+    }
+    a.init();
+}(window));
+
 if(!document.querySelector('script[src^="https://player.pandavideo.com.br/api.v2.js"]')){
     let s=document.createElement('script'); 
     s.src='https://player.pandavideo.com.br/api.v2.js'; 
