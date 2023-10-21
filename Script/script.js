4850f69abd46acdd2cb0b662e434686723bf5701
Joanibi Ribeiro <101252091+Joanibi@users.noreply.github.com>
2023-10-20 22:16:43 -0300

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,26 @@
+/*  
+window.onload = function () {
+    window.addEventListener("contextmenu", e => e.preventDefault());
+} 
+*/
+if(!document.querySelector('script[src^="https://player.pandavideo.com.br/api.v2.js"]')){
+    let s=document.createElement('script'); 
+    s.src='https://player.pandavideo.com.br/api.v2.js'; 
+    s.async=true; document.head.appendChild(s);
+}
+window.pandascripttag=window.pandascripttag || [];
+window.pandascripttag.push(function (){
+    const p=new PandaPlayer('panda-8373ac3c-0f59-4581-be0f-5e22eabc5c53',{
+        onReady(){p.loadButtonInTime({fetchApi: true})}});
+})
+function curtiu(number) {
+    if (document.getElementById('like_'+number).innerHTML=="Curtir") {
+        document.getElementById('like_'+number).innerHTML = "Descurtir";
+        document.getElementById('like_'+number).style.color = "#365899";
+        document.getElementById('qtd_like_'+number).innerHTML++;
+    } else {
+        document.getElementById('like_'+number).innerHTML = "Curtir";
+        document.getElementById('like_'+number).style.color = "#93979f";
+        document.getElementById('qtd_like_'+number).innerHTML--;
+    }
+}
