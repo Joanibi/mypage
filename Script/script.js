5f5dd8ddbbd3d74efd7afb37a5f0759b8034c05e
Joan Ribeiro <101252091+Joanibi@users.noreply.github.com>
2023-10-25 12:39:32 -0300

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,13 @@
 window.onload = function () {
     window.addEventListener("contextmenu", e => e.preventDefault());
+
+    var links = document.getElementsByTagName("a");
+    for (var i = 0, n = links.length; i < n; i++) {
+        var href = links[i].href.trim() +
+        (links[i].href.indexOf("?") > 0 ? '&' : '?') +
+        document.location.search.replace('?', '').toString();
+        links[i].href = href;
+    }
 } 
 
 (function(b) {
